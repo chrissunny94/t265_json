aefc491aa8a00d0ac6e840fc74a2af6ea2a46be4
chrissunny94 <chrissunny94@gmail.com>
2021-02-28 16:40:43 +0530

    Updated readme and added install scripts

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # t265_json
 
 
-## Clone & build using catkin
+
+
+## Clone & install ros
 
 ```
 mkdir ~/catkin_ws/src -p
 cd ~/catkin_ws/src
 git clone  https://github.com/chrissunny94/t265_json
+cd t265_json
+./install_ros_melodic_and_realsense_drivers_for_ubuntu.sh
+```
+
+
+
+## Build the workspace (build using catkin) 
+
+
+```
 cd ../../
 catkin build
 source devel/setup.bash
---
+++ install_ros_melodic_and_realsense_drivers_for_ubuntu.sh
@@ -0,0 +1,18 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install ros-melodic-desktop-full
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep
+sudo rosdep init
+rosdep update
+
+
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo apt-get install librealsense2-dkms
+sudo apt-get install librealsense2-utils
+sudo apt install ros-noetic-realsense2-*
+sudo apt install ros-noetic-librealsense2
