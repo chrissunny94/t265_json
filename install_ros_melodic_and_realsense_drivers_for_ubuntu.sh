401c5b643288efc8f26e52f7f9530d08b9db864d
chrissunny94 <chrissunny94@gmail.com>
2021-03-11 18:57:13 +0530

    Changes in script and permissions

--- install_ros_melodic_and_realsense_drivers_for_ubuntu.sh
+++ install_ros_melodic_and_realsense_drivers_for_ubuntu.sh
@@ -1,20 +1,22 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install ros-melodic-desktop-full -y
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install python-rosdep
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+sudo apt install python-rosdep -y
+sudo pip3 install rospkg
 sudo rosdep init
 rosdep update
-sudo apt install python-catkin-tools
+sudo apt install python-catkin-tools -y
 
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
-sudo apt-get install librealsense2-dkms
-sudo apt-get install librealsense2-utils
-sudo apt install ros-noetic-realsense2-*
-sudo apt install ros-noetic-librealsense2
+sudo apt-get install librealsense2-dkms -y
+sudo apt-get install librealsense2-utils -y
+sudo apt install ros-noetic-realsense2-* -y
+sudo apt install ros-noetic-librealsense2 -y
+sudo apt install -f
 
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
